c5c33c225f4fd0eee3a1a2be51363dd25945ab88
cpan001 <github email address>
2017-09-11 16:49:52 -0400

    add test.rb

---
+++ test.rb
@@ -0,0 +1,14 @@
+require 'food2fork'
+require 'pry'
+require 'unirest'
+require 'httparty'
+require 'json'
+
+# These code snippets use an open-source library. http://unirest.io/ruby
+response = Unirest.get "https://community-food2fork.p.mashape.com/get?key=efe3f39d1fcfa825711f0d701a410a08&rId=35171&rId=35171",
+  headers:{
+    "X-Mashape-Key" => "W4A6kuCD3xmsh2xY1vQbzRAlK9bAp1yGHrDjsn9sCRAOaBtNu7",
+    "Accept" => "application/json"
+  }
+
+puts response.body
